7c352e78698b69e77de262ff1347042d51f57c1c
AkashSinghChouhan <37949976+AkashSinghChouhan@users.noreply.github.com>
2020-10-20 20:35:45 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# NotesApp
-Simple notes management app using NodeJs, Express and MongoDB (without UI).
+# nodecode
+
+
+*Technology used:-*
+Node js + Express js + MySql
+
+
+Required node packages:-
+npm
+moment
+express
+mysql
+
+*For Database setup:*
+
+Please run the attached sql script file on mysql workbench
+
+For DB connetion go to   NotesApp\routes\notes.js and inside this file add databese details
+
+eg:-
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'test'
+
+
+*For runnig the project go to the main directory containing 'app.js' and run the following command-:*
+
+npm run
+
+
+*API endpoints for testing the app:-*
+
+for get all notes
+get- http://localhost:3000/notes/api/getNotes
+
+for post/create notes
+post- http://localhost:3000/notes/api/insertNotes
+
+for update the notes
+put- http://localhost:3000/notes/api/updateNotes/id
+
+for select perticular note
+get- http://localhost:3000/notes/api/getById/id
+
+for deleting the note
+delete- http://localhost:3000/notes/api/deleteById/id
+
+for notes history
+get- http://localhost:3000/notes/api/history/id
+
+
+
+
---
+++ repo/notesrepo.js
@@ -0,0 +1,56 @@
+var mysql = require('mysql');
+
+
+
+exports.getnotes = (res) => {
+    return new Promise((resolve, reject) => {
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "root",
+            database: "test"
+          });
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+            var sql = "select * from test.notes";
+            console.log("______________________________********", sql);
+            con.query(sql, function (err, result) {
+              if (err) throw err;
+              console.log("1 record inserted", result);
+            });
+          });
+        
+        return resolve(result)
+
+    })
+}
+
+
+exports.insertNotes = (data) => {
+    return new Promise((resolve, reject) => {
+        var con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "root",
+            database: "test"
+          });
+        con.connect(function(err) {
+            if (err) throw err;
+            var title = data.title;
+            var content = data.content
+            
+
+            console.log("Connected!");
+            var sql = `INSERT INTO test.notes (title, content, created_date, updated_date ) VALUES (${title}, '${content}','2008-01-01 00:00:01','2008-01-01 00:00:01');`;
+            console.log("______________________________********", sql);
+            con.query(sql, function (err, result) {
+              if (err) throw err;
+              console.log("1 record inserted", result);
+            });
+          });
+        
+        return resolve(result)
+
+    })
+}
